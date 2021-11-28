3f1cce0f3404de24df0e9a1c44fc31935862f643
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-11-28 13:38:51 -0300

    adicionado um validador para o campo 'data'

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-03-javascript-eventos/dia-01-html-css-forms/exercicio/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-03-javascript-eventos/dia-01-html-css-forms/exercicio/script.js
@@ -24,11 +24,51 @@ function isInvalidName() {
   return false
 }
 
+function isInvalidDate() {
+  const input = document.querySelector('#date').value
+  // Regex para buscar o formato dd/mm/aaa
+  const regexCatchFormat = /^(\d{2}\/){2}\d{4}$/
+  // Regex para buscar um intervalo de numero de (01 a 31)
+  const regexCatchDay = /^(3[0-1]|[1-2]\d|[0][1-9])\//
+  // Regex para buscar um intervalo de numero de (01 a 12)
+  const regexCatchMonth = /\/(1[0-2]|0[1-9])\//
+  // Regex para buscar um intervalo de numero de (1908 a 2021)
+  const regexCatchYear = /\/(190[8-9]|19[1-9]\d|20[0-1]\d|202[0-1])$/
+  let isInvalid = false
+
+  if (!regexCatchFormat.test(input)) {
+    isInvalid = true
+    alert('Formato da data inválida!')
+    return;
+  }
+
+  if (!regexCatchDay.test(input)) {
+    isInvalid = true
+    alert('Dia inválido!')
+    return;
+  }
+
+  if (!regexCatchMonth.test(input)) {
+    isInvalid = true
+    alert('Mês inválido!')
+    return;
+  }
+
+  if (!regexCatchYear.test(input)) {
+    isInvalid = true
+    alert('Ano inválido!')
+    return;
+  }
+
+  return isInvalid
+}
+
 function submitForm(event) {
   event.preventDefault()
   console.log('event', event.target)
 
   if(isInvalidName()) return;
+  if(isInvalidDate()) return;
 }
 
 btnSubmit.addEventListener('click', submitForm)
