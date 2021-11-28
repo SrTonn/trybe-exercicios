82c819748eebc89ec767b2bb0c7bcaba95224d6c
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-11-28 13:38:14 -0300

    adicionado um validador para o campo 'nome'

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-03-javascript-eventos/dia-01-html-css-forms/exercicio/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-03-javascript-eventos/dia-01-html-css-forms/exercicio/script.js
@@ -1,5 +1,7 @@
 const selectState = document.querySelector('#state')
 const stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+const btnSubmit = document.querySelector('#submit')
+const divAnswer = document.querySelector('#answer')
 
 function createTagOptions(state) {
   const option = document.createElement('option')
@@ -7,6 +9,26 @@ function createTagOptions(state) {
   option.value = state
   selectState.appendChild(option)
 }
+
 stateOptions.forEach((state) => {
   createTagOptions(state)
 })
+
+function isInvalidName() {
+  const input = document.querySelector('#name').value
+
+  if (!input) {
+    alert('O campo "nome" está vazio!')
+    return true
+  }
+  return false
+}
+
+function submitForm(event) {
+  event.preventDefault()
+  console.log('event', event.target)
+
+  if(isInvalidName()) return;
+}
+
+btnSubmit.addEventListener('click', submitForm)
