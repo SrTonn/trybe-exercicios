27b2818eb28db2887e83445f98e49739d7b8d79e
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-11-29 14:56:01 -0300

    codigo refatorado para criar uma lista de erros

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-03-javascript-eventos/dia-01-html-css-forms/exercicio/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-03-javascript-eventos/dia-01-html-css-forms/exercicio/script.js
@@ -2,6 +2,8 @@ const selectState = document.querySelector('#state')
 const stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 const btnSubmit = document.querySelector('#submit')
 const divAnswer = document.querySelector('#answer')
+const tagUl = document.createElement('ul')
+let errorList = []
 
 function createTagOptions(state) {
   const option = document.createElement('option')
@@ -14,98 +16,84 @@ stateOptions.forEach((state) => {
   createTagOptions(state)
 })
 
+function createLi(liContent, parentNode = tagUl) {
+  const li = document.createElement('li')
+  li.innerText = liContent
+  li.className = 'incorrectAnswer'
+  parentNode.appendChild(li)
+}
+
 function isInvalidName() {
   const input = document.querySelector('#name')
+  const msgError = 'O campo "nome" está vazio!'
 
-  if (!input.value) {
-    alert('O campo "nome" está vazio!')
+  const regexCatchAnyLetter = /[a-z]+/i
+  
+  if (!regexCatchAnyLetter.test(input.value)) {
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true
   }
+
   return false
 }
 
 function isInvalidEmail() {
   const input = document.querySelector('#email')
   const regexCatchEmailFormat = /^\w+@[a-z]+\.[a-z]{2,3}$/i
-
+  
   if (!regexCatchEmailFormat.test(input.value)) {
-    alert('Formato Inválido!\nTente o seguinte formato: nome@dominio.com')
+    const msgError = 'Formato Inválido! Tente o seguinte formato: nome@dominio.com'
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true
   }
 
   if (!input.value) {
-    alert('Campo "e-mail" obrigatório!')
+    const msgError = 'Campo "e-mail" obrigatório!'
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true
   }
-  return false
-}
 
-function isInvalidDate() {
-  const input = document.querySelector('#date')
-  // Regex para buscar o formato dd/mm/aaa
-  const regexCatchFormat = /^(\d{2}\/){2}\d{4}$/
-  // Regex para buscar um intervalo de numero de (01 a 31)
-  const regexCatchDay = /^(3[0-1]|[1-2]\d|[0][1-9])\//
-  // Regex para buscar um intervalo de numero de (01 a 12)
-  const regexCatchMonth = /\/(1[0-2]|0[1-9])\//
-  // Regex para buscar um intervalo de numero de (1908 a 2021)
-  const regexCatchYear = /\/(190[8-9]|19[1-9]\d|20[0-1]\d|202[0-1])$/
-
-  if (!regexCatchFormat.test(input.value)) {
-    alert('Formato da data inválida!')
-    input.focus()
-    return true;
-  }
-
-  if (!regexCatchDay.test(input.value)) {
-    alert('Dia inválido!')
-    input.focus()
-    return true;
-  }
-
-  if (!regexCatchMonth.test(input.value)) {
-    alert('Mês inválido!')
-    input.focus()
-    return true;
-  }
-
-  if (!regexCatchYear.test(input.value)) {
-    alert('Ano inválido!')
-    input.focus()
-    return true;
-  }
-  
   return false
 }
 
 function isInvalidCpf() {
   const input = document.querySelector('#cpf')
-  
+
   const regexCatch11Number = /^\d{11}$/
+  const msgError = 'Cpf inválido!'
   
   if(!regexCatch11Number.test(input.value)) {
-    alert('Cpf inválido!')
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true;
   }
+
   return false;
 }
 
 function isInvalidAddres() {
   const input = document.querySelector('#addres')
-  const regexCatchMinWord = /^[a-z]{2,10}\.?\s\w{2,}/i
-
+  const regexCatchMinWord = /^[a-z]{2,10}\.?\s\w{1,}/i
+  
   if (!regexCatchMinWord.test(input.value)) {
-    alert('Endereço incompleto!')
+    const msgError = 'Endereço incompleto!'
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true;
   }
-
+  
   if (!input.value) {
-    alert('Campo de endereço vazio!')
+    const msgError = 'Campo de endereço vazio!'
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true;
   }
@@ -116,15 +104,19 @@ function isInvalidAddres() {
 function isInvalidCity() {
   const input = document.querySelector('#city')
   const regexCatchMinWord = /[a-z]{2,}/i
-
+  
   if (!input.value) {
-    alert('O campo "cidade" está vazio!')
+    const msgError = 'O campo "cidade" está vazio!'
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true
   }
-
+  
   if (!regexCatchMinWord.test(input.value)) {
-    alert('Cidade inválida!')
+    const msgError = 'Cidade inválida!'
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true;
   }
@@ -134,9 +126,11 @@ function isInvalidCity() {
 
 function isInvalidState() {
   const input = document.querySelector('#state')
+  const msgError = 'Selecione um estado!'
 
   if(input.value === '--') {
-    alert('Selecione um estado!')
+    alert(msgError)
+    errorList.push(msgError)
     input.focus()
     return true;
   }
@@ -151,20 +145,79 @@ function complementChosen() {
   return radixAp.checked ? radixAp.value : radixHouse.value
 }
 
+function isInvalidDate() {
+  const input = document.querySelector('#date')
+  // Regex para buscar o formato dd/mm/aaa
+  const regexCatchFormat = /^(\d{2}\/){2}\d{4}$/
+  // Regex para buscar um intervalo de numero de (01 a 31)
+  const regexCatchDay = /^(3[0-1]|[1-2]\d|[0][1-9])\//
+  // Regex para buscar um intervalo de numero de (01 a 12)
+  const regexCatchMonth = /\/(1[0-2]|0[1-9])\//
+  // Regex para buscar um intervalo de numero de (1908 a 2021)
+  const regexCatchYear = /\/(190[8-9]|19[1-9]\d|20[0-1]\d|202[0-1])$/
+
+  if (!regexCatchFormat.test(input.value)) {
+    const msgError = 'Formato da data inválida!'
+    alert(msgError)
+    errorList.push(msgError)
+    input.focus()
+    return true;
+  }
+
+  if (!regexCatchDay.test(input.value)) {
+    const msgError = 'Dia inválido!'
+    alert(msgError)
+    errorList.push(msgError)
+    input.focus()
+    return true;
+  }
+
+  if (!regexCatchMonth.test(input.value)) {
+    const msgError = 'Mês inválido!'
+    alert(msgError)
+    errorList.push(msgError)
+    input.focus()
+    return true;
+  }
+
+  if (!regexCatchYear.test(input.value)) {
+    const msgError = 'Ano inválido!'
+    alert(msgError)
+    errorList.push(msgError)
+    input.focus()
+    return true;
+  }
+
+  return false
+}
+
 function submitForm(event) {
   event.preventDefault()
 
-  if(isInvalidName()) return;
-  if(isInvalidEmail()) return;
-  if(isInvalidCpf()) return;
-  if(isInvalidAddres()) return;
-  if(isInvalidCity()) return;
-  if(isInvalidState()) return;
-  // complementChosen()
+  const allConditions = [
+    isInvalidName(),
+    isInvalidEmail(),
+    isInvalidCpf(),
+    isInvalidAddres(),
+    isInvalidCity(),
+    isInvalidState(),
+    isInvalidDate(),
+  ]
 
-  if(isInvalidDate()) return;
+  errorList = [...new Set(errorList)]
+  errorList.forEach(error => {
+    createLi(error)
+  })
 
-  divAnswer.innerText = 'Formulario enviado!'
+  divAnswer.innerHTML = allConditions.includes(true)
+                      ? tagUl.outerHTML
+                      : 'Formulario enviado!'
+
+  errorList = []
+  tagUl.innerHTML = ''
 }
 
 btnSubmit.addEventListener('click', submitForm)
+
+// https://www.javascripttutorial.net/array/javascript-remove-duplicates-from-array/
+// utilizado o "Set" para remover elementos repetidos de um array
