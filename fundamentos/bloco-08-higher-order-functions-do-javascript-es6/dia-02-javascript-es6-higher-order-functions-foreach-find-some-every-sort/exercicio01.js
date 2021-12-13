e8f8fc8cb47c874efa269f273dff3b99070be402
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-13 16:21:30 -0300

    exercicio 01 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
@@ -60,3 +60,10 @@ const books = [
     releaseYear: 1928,
   },
 ];
+
+function authorBornIn1947(array) {
+  const { author } = array.find(({author}) => author.birthYear === 1947)
+  
+  return author.name
+}
+console.log(authorBornIn1947(books)) // Stephen King
