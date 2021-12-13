8f4bdab116484ddadfd606ec9bf6b25519ab647d
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-13 16:34:40 -0300

    exercicio 05 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
@@ -151,3 +151,11 @@ function booksOrderedByReleaseYearDesc(array) {
   return array.sort((a, b) => b.releaseYear - a.releaseYear)
 }
 console.log(booksOrderedByReleaseYearDesc(books))
+
+// Exercício 05
+// const expectedResult = false;
+
+function everyoneWasBornOnSecXX(array) {
+  return array.every(({author}) => author.birthYear > 2001)
+}
+console.log(everyoneWasBornOnSecXX(books)) // false
