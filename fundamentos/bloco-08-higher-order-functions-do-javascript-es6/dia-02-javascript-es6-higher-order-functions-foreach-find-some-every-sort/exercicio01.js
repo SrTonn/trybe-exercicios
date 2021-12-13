d0e5d8bf33d2669b6f38e6fbb5990382c5f9432c
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-13 16:26:40 -0300

    exercicio 02 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
@@ -67,3 +67,19 @@ function authorBornIn1947(array) {
   return author.name
 }
 console.log(authorBornIn1947(books)) // Stephen King
+
+function smallerName(array) {
+  let nameBook;
+
+  // escreva aqui o seu código
+  array.forEach(({name}, i) => {
+    if (i === 0 || nameBook.length > name.length) {
+      nameBook = name
+    }
+  })
+
+  // Variável nameBook que receberá o valor do menor nome;
+  return nameBook;
+}
+
+console.log(smallerName(books)) // Duna
