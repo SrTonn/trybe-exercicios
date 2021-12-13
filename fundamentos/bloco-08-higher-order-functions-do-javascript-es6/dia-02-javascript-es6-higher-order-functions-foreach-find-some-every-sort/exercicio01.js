898abcaf4f7d73d1f96b6798774799607d7790a6
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-13 16:29:52 -0300

    exercicio 03 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio01.js
@@ -83,3 +83,19 @@ function smallerName(array) {
 }
 
 console.log(smallerName(books)) // Duna
+
+// Exercício 03
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: { name: 'George R. R. Martin', birthYear: 1948 },
+  releaseYear: 1991,
+};
+
+function getNamedBook(array) {
+  // escreva seu código aqui
+  return array.find(({name}) => name.length === 26)
+}
+
+console.log(getNamedBook(books))
