76fee7b1d705d9735b23d721699ba2e4daaf260b
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-14 18:31:32 -0300

    exercicio 02 parte 02 concluido

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio02.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio02.js
@@ -1,5 +1,9 @@
-const recursive = num => {
-  return num <= 1 ? 1 : num * recursive(num - 1)
-}
+// Parte I
+const recursive = num => num <= 1 ? 1 : num * recursive(num - 1)
+console.log(recursive(4), 24)
 
-console.log(recursive(5), 24)
+// Parte II
+const longestWord = (str) => str.split(' ').reduce((acc, val) => acc.length > val.length ? acc : val)
+const res = longestWord("Antonio foi no banheiro e nao sabemos o que aconteceu") // retorna 'aconteceu'
+
+console.log(res) // retorna 'aconteceu'
