739ec00babe0eecbad23619ec9bf55bdbb77e068
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-24 19:51:21 -0300

    exercicio 01 concluido

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
@@ -0,0 +1,33 @@
+// exercicio 1
+
+const adicao = (num1, num2) => num1 + num2
+const substracao = (num1, num2) => num1 - num2
+const multiplicacao = (num1, num2) => num1 * num2
+const divisao = (num1, num2) => num1 / num2
+const modulo = (num1, num2) => num1 % num2
+
+// exercicio 2
+
+// exercicio 4
+
+// exercicio 5
+
+// exercicio 6
+
+// exercicio 7
+
+// exercicio 8
+
+// exercicio 9
+
+// exercicio 10
+
+// exercicio 11
+
+// exercicio 12
+
+// exercicio 13
+
+// exercicio 14
+
+// exercicio 15
