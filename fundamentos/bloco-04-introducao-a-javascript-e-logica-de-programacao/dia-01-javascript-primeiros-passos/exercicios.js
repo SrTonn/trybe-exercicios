69fdb8b76578bb2e42d1ab69351408202a041b0f
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-12-25 00:58:48 -0300

    exercicio 11 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
@@ -72,10 +72,33 @@ console.log(calcProfit(1, 3))
 
 // exercicio 11
 
-// exercicio 12
+const aliquotInssAndIr = (grossSalary) => {
+  let aliquotINSS
+  let aliquotIR
 
-// exercicio 13
+  if (grossSalary <= 1556.94) {
+    aliquotINSS = grossSalary * 0.08
+  } else if (grossSalary <= 2594.92) {
+    aliquotINSS = grossSalary * 0.09
+  } else if (grossSalary <= 5189.82) {
+    aliquotINSS = grossSalary * 0.11
+  } else {
+    aliquotINSS = 570.88
+  }
 
-// exercicio 14
+  const baseSalary = grossSalary - aliquotINSS
 
-// exercicio 15
+  if (baseSalary <= 1903.98) {
+    aliquotIR = 0
+  } else if (baseSalary <= 2826.65) {
+    aliquotIR = baseSalary * 0.075 - 142.8
+  } else if (baseSalary <= 3751.05) {
+    aliquotIR = baseSalary * 0.15 - 354.8
+  } else if (baseSalary <= 4664.68) {
+    aliquotIR = baseSalary * 0.225 - 636.13
+  } else {
+    aliquotIR = baseSalary * 0.275 - 869.36
+  }
+
+  return 'Salário: ' + (baseSalary - aliquotIR)
+}
