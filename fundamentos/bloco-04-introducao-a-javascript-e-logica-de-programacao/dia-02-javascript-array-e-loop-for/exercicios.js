da37f7cf1b146ee9cc405b1965b866e4ece19bd1
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-03 10:13:50 -0300

    requisito 01 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -1,1 +1,14 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// exercicio 01
+
+numbers.forEach(num => console.log(num))
+
+// exercicio 02
+// exercicio 03
+// exercicio 04
+// exercicio 05
+// exercicio 06
+// exercicio 07
+// exercicio 08
+// exercicio 09
