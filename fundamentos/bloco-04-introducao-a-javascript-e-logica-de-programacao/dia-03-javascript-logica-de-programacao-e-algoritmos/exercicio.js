b2e8c5e78f33711decc84ca94ae0e983535bfa3e
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-03 10:56:10 -0300

    requisito 01 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -1,4 +1,15 @@
 // Exercicio 01
+
+// O fatorial é representado pelo sinal !
+// 4! = 4 x 3 x 2 x 1 = 24
+
+const val = 10
+let acc = val;
+for (let i = val - 1; i > 0; i -= 1) {
+  acc *= i
+}
+console.log(acc)
+
 // Exercicio 02
 // Exercicio 03
 // Exercicio 04
