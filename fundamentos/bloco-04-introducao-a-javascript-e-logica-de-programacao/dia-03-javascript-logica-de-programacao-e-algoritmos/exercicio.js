697ddd3f04f6de3fa2bd7bb1a065567d73835481
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-03 11:10:14 -0300

    requisito 04 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio.js
@@ -27,3 +27,19 @@ const smallestWord = array.sort((a, b) => a.length - b.length)[0]
 console.log(smallestWord)
 
 // Exercicio 04
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
