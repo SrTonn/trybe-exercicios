79c7c49cf727e0ea2567ce297b9d8dc626afeacf
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-04 13:44:13 -0300

    exercicio 06 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio-06.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio-06.js
@@ -2,8 +2,12 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  return students.reduce((acc, name, i) => {
+    const average = grades[i].reduce((a, n) => a+=n ) / grades[i].length
+    return [...acc, {name, average}]
+  }, [])
 }
+console.log(studentAverage())
 
 const expected = [
   { name: 'Pedro Henrique', average: 7.8 },
