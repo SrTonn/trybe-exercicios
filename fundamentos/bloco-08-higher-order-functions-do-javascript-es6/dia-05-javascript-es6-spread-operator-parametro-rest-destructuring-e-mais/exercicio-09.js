4acc0651957e4fd094e2b950145faacb06327672
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-04 20:13:54 -0300

    exercicio 09 adicionado

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio-09.js
@@ -0,0 +1,6 @@
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
