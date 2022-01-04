41f7bd10b1037819a6405342940032d48f1e364b
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-04 20:05:44 -0300

    exercicio 06 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio-06.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio-06.js
@@ -3,3 +3,5 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
+const toObject = ([name, brand, year]) => ({ name, brand, year });
+console.log(toObject(palio))
