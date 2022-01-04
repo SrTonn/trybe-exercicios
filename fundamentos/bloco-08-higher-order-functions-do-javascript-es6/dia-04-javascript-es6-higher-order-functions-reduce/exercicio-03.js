3bfd7f0439096348fb4b8d92be212b86924259e1
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-04 13:18:52 -0300

    exercicio 03 concluido

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio-03.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio-03.js
@@ -66,5 +66,6 @@ const books = [
 const expectedResult = 43;
 
 function averageAge() {
-  // escreva seu código aqui
+  return books.reduce((acc, val) => (val.releaseYear - val.author.birthYear) + acc, 0) / books.length
 }
+console.log(averageAge())
