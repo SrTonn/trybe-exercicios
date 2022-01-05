b2126743c0bda6eae958a6991c925378186c344f
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-05 10:06:58 -0300

    exercicio 05 refatorado

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio-05.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercicio-05.js
@@ -5,6 +5,6 @@ const names = [
 ];
 
 function containsA() {
-  return names.reduce((acc, val) => acc += val.match(/a/ig).length, 0)
+  return names.reduce((acc, val) => acc + val.match(/a/ig).length, 0)
 }
 console.log(containsA()) // 20
