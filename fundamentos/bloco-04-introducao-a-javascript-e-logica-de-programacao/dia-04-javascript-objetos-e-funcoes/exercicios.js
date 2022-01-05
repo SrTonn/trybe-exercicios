6a816515754dbcc70338412da91c90530b8d957e
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-04 21:08:38 -0300

    exercicio 05 concluido

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -26,6 +26,26 @@ for (const key in info) {
 }
 
 // Exercícios 05
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let properties in info) {
+  if (
+    properties === 'recorrente' &&
+    info[properties] === 'Sim' &&
+    info2[properties] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[properties] + ' e ' + info2[properties]);
+  }
+}
+
 // Exercícios 06
 // Exercícios 07
 // Exercícios 08
