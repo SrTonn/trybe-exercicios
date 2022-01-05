cb7478c54f2c52a54f1d0fc74b36795e949029b5
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-04 21:00:54 -0300

    exercicios adicionados

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios.js
@@ -0,0 +1,14 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// Exercícios 01
+// Exercícios 02
+// Exercícios 03
+// Exercícios 04
+// Exercícios 05
+// Exercícios 06
+// Exercícios 07
+// Exercícios 08
