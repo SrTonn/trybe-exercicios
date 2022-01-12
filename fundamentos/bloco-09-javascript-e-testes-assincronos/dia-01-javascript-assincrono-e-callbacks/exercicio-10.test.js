cf3dac031e13f107299515fd3fb683be6ff968a7
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-12 11:15:38 -0300

    exercicio 10 concluido

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicio-10.test.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicio-10.test.js
@@ -1,11 +1,11 @@
-beforeEach(() => console.log('1 - beforeEach'));
-afterEach(() => console.log('1 - afterEach'));
+beforeEach(() => console.log('1 - beforeEach')); // 1, 4
+afterEach(() => console.log('1 - afterEach')); // 3, 8
 
-test('', () => console.log('1 - test'));
+test('', () => console.log('1 - test')); // 2
 
 describe('Scoped / Nested block', () => {
-  beforeEach(() => console.log('2 - beforeEach'));
-  afterEach(() => console.log('2 - afterEach'));
+  beforeEach(() => console.log('2 - beforeEach')); // 5
+  afterEach(() => console.log('2 - afterEach')); // 7
 
-  test('', () => console.log('2 - test'));
+  test('', () => console.log('2 - test')); // 6
 });
