0649b9cbd090f43c16665de053dc3e15ede9d7cc
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-21 15:43:30 -0300

    exercicio 01 e 02 concluidos

--- 02-frond-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicio-01/src/App.js
+++ 02-frond-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicio-01/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+const tasks = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ol>
+        { tasks.map((task) => Task(task)) }
+      </ol>
   );
 }
 
