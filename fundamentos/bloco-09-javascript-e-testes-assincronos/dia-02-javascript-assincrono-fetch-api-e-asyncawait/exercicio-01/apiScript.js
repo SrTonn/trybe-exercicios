8df7c6fc717251cca6d5ff3d57c26e5ffc2b40cc
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-24 18:49:46 -0300

    exercicio 01 concluido

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/exercicio-01/apiScript.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-asyncawait/exercicio-01/apiScript.js
@@ -2,6 +2,17 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { Accept: 'application/json' },
+  };
+
+  return fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => data);
 };
 
-window.onload = () => fetchJoke();
+window.onload = async () => {
+  const { joke } = await fetchJoke();
+  document.querySelector('h1').innerText = joke;
+};
